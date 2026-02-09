dc2c96c1fd58a8ae3021d9c3d87c1a17b2476337
Rushikesh <hrishi210402@gmail.com>
2026-02-09 11:23:54 +0530

    sitemaps change 1

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -11,19 +11,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}/#about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/projects`,
+      url: `${baseUrl}/#projects`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/contact`,
+      url: `${baseUrl}/#contact`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.7,
